fc516c2e07436f25dbe878c34a90b1f75a752fd3
Guritno Andri Zulverdi <31233367+gorezeus@users.noreply.github.com>
2025-08-22 17:26:35 +0700

    Add MediatR Add AutoMapper Add Features (CQRS) for LeaveType Add some DTO Add Custom Exception Add MappingProfiles Add ServiceRegistration

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
+{
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+    {
+        private readonly Mapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public UpdateLeaveTypeCommandHandler(Mapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // validation
+
+            // convert
+            var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
+
+            // update to database
+            await _leaveTypeRepository.UpdateAsync(leaveTypeToUpdate);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly Mapper _mapper;
+
+    public GetLeaveTypesQueryHandler(ILeaveTypeRepository leaveTypeRepository, Mapper mapper)
+    {
+        _leaveTypeRepository = leaveTypeRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        var data = await _leaveTypeRepository.GetAsync();
+        return _mapper.Map<List<LeaveTypeDto>>(data);
+    }
+}
