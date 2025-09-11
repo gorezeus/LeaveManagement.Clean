cae6e35299742f997688ad3358949c5b251cf2e1
Guritno Andri Zulverdi <31233367+gorezeus@users.noreply.github.com>
2025-09-11 17:08:51 +0700

    Change the IGenericRepository generic type from class to BaseEntity

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HR.LeaveManagement.Domain.Common;
 
 namespace HR.LeaveManagement.Application.Contracts.Persistence;
-public interface IGenericRepository<T> where T : class
+public interface IGenericRepository<T> where T : BaseEntity
 {
     Task<IReadOnlyList<T>> GetAsync();
     Task<T> GetByIdAsync(int id);
