365be2356a205b8264f43e0b8e0b5f77f4eb4e1a
Guritno Andri Zulverdi <31233367+gorezeus@users.noreply.github.com>
2025-09-17 19:39:58 +0700

    Create GetLeaveTypeQueryHandlerTests

---
+++ HR.LeaveManagement.Application.UnitTests/Features/LeaveTypes/Queries/GetLeaveTypeQueryHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Castle.Core.Logging;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.MappingProfiles;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Shouldly;
+
+namespace HR.LeaveManagement.Application.UnitTests.Features.LeaveTypes.Queries;
+public class GetLeaveTypeQueryHandlerTests
+{
+    private readonly Mock<ILeaveTypeRepository> _mockRepo;
+    private readonly IMapper _mapper;
+    private readonly Mock<IAppLogger<GetLeaveTypesQueryHandler>> 
+        _mockAppLogger;
+
+    public GetLeaveTypeQueryHandlerTests()
+    {
+        _mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+
+        var mapperConfig = new MapperConfiguration(c =>
+        {
+            c.AddProfile<LeaveTypeProfile>();
+        }, new NullLoggerFactory());
+        _mapper = mapperConfig.CreateMapper();
+
+        _mockAppLogger = new Mock<IAppLogger<GetLeaveTypesQueryHandler>>();
+    }
+
+    [Fact]
+    public async Task GetLeaveTypeListTest()
+    {
+        // Arrange
+        var handler = new GetLeaveTypesQueryHandler(_mockRepo.Object, 
+            _mapper,  
+            _mockAppLogger.Object);
+
+        // Act
+        var result = await handler.Handle(
+            new GetLeaveTypesQuery(), 
+            CancellationToken.None);
+
+        // Assert
+        result.ShouldBeOfType<List<LeaveTypeDto>>();
+        result.Count.ShouldBe(3);
+    }
+}
